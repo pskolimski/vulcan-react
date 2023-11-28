88c2ef580165b8ed63cbf70b9f26c68947b94a9e
Patryk Skolimowski <zsmi@revku.dev>
2023-11-28 10:33:36 +0100

    Add basic students data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import studentsList from 'data/students.json';
 
 const App = () => {
+   const [students] = useState(studentsList);
+
    return (
       <>
          <h1>Hello world</h1>
